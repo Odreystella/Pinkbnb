ab7ffbaef8daaf035c9ace79450ca0a006bb77a8
Oderystella <ddott59@gmail.com>
2021-08-31 20:45:23 +0900

    feat: register User to admin site with decorator
    - use fieldsets to add custom profile

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,43 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from . import models
 
-# Register your models here.
+
+# admin.ModelAdmin을 상속받는 경우
+# @admin.register(models.User)
+# class CustomUserAdmin(admin.ModelAdmin):
+
+#     """ Custom User Admin """
+
+#     list_display = ("username", "email", "gender", "language", "currency", "superhost")
+#     list_filter = (
+#         "language",
+#         "currency",
+#         "superhost",
+#     )
+
+# 방법 1
+@admin.register(models.User)  # 데코레이터를 붙여 주면 CustomUserAdmin 클래스가 models.User를 사용한다는 의미
+class CustomUserAdmin(UserAdmin):
+
+    """Custom User Admin"""
+
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Custom Profile",
+            {
+                "fields": (
+                    "avatar",
+                    "gender",
+                    "bio",
+                    "birthdate",
+                    "language",
+                    "currency",
+                    "superhost",
+                )
+            },
+        ),
+    )
+
+
+# admin.site.register(models.User, CustomUserAdmin)  # 방법 2
