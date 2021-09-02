0425a721f84d7c7e6b52f3769c1a621ce9d447ef
Oderystella <ddott59@gmail.com>
2021-09-02 23:37:15 +0900

    feat: make roomadmin better

--- rooms/admin.py
+++ rooms/admin.py
@@ -13,7 +13,69 @@ class ItemAdmin(admin.ModelAdmin):
 class RoomAdmin(admin.ModelAdmin):
     """Room Admin Definition"""
 
-    pass
+    fieldsets = (
+        (
+            "Basic Info",
+            {
+                "fields": (
+                    "name",
+                    "description",
+                    "room_type",
+                    "country",
+                    "city",
+                    "address",
+                    "price",
+                )
+            },
+        ),
+        ("Times", {"fields": ("check_in", "check_out", "instant_book")}),
+        ("Spaces", {"fields": ("guests", "beds", "bedrooms", "baths")}),
+        (
+            "More About the Space",
+            {
+                "classes": ("collapse",),
+                "fields": (
+                    "amenities",
+                    "facilities",
+                    "house_rules",
+                ),
+            },
+        ),
+        ("Last Details", {"fields": ("host",)}),
+    )
+    list_display = (
+        "name",
+        "country",
+        "city",
+        "price",
+        "guests",
+        "beds",
+        "baths",
+        "bedrooms",
+        "check_in",
+        "check_out",
+        "instant_book",
+    )
+    ordering = ("instant_book",)
+    list_filter = (
+        "instant_book",
+        "host__superhost",
+        "room_type",
+        "amenities",
+        "facilities",
+        "house_rules",
+        "city",
+        "country",
+    )
+    search_fields = (
+        "city",
+        "^host__username",
+    )
+    filter_horizontal = (
+        "amenities",
+        "facilities",
+        "house_rules",
+    )
 
 
 @admin.register(Photo)
