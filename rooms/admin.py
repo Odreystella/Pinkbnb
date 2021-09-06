55d18e46d11d9f64dd437e317c320eac91041818
Oderystella <ddott59@gmail.com>
2021-09-07 00:20:58 +0900

    feat: view thumbnail on admin panel of Photo

--- rooms/admin.py
+++ rooms/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.html import mark_safe
 from .models import Room, RoomType, Amenity, Facility, HouseRule, Photo
 
 
@@ -98,6 +99,12 @@ class RoomAdmin(admin.ModelAdmin):
 
 @admin.register(Photo)
 class PhotoAdmin(admin.ModelAdmin):
-    """ """
 
-    pass
+    """Photo Admin Definition"""
+
+    list_display = ("__str__", "get_thumbnail")
+
+    def get_thumbnail(self, obj):
+        return mark_safe(f"<img width='50px' height='50px' src='{obj.file.url}' />")
+
+    get_thumbnail.short_description = "Thumbnail"
