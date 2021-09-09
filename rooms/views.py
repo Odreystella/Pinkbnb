c2a974d046ae74c7a9ce2ad6a3c95601e994aa16
Oderystella <ddott59@gmail.com>
2021-09-10 01:24:23 +0900

    feat: create pagination using Paginator

--- rooms/views.py
+++ rooms/views.py
@@ -1,25 +1,17 @@
 from math import ceil
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from .models import Room
 
 
 def enter_home(request):
-    page = request.GET.get("page", 1)
-    page = int(page or 1)
-    page_size = 10
-    limit = page_size * page
-    offset = limit - page_size
-    all_rooms = Room.objects.all()[offset:limit]
-
-    page_count = ceil(Room.objects.count() / page_size)
-    page_range = range(1, page_count + 1)
+    page_num = request.GET.get("page", 1)
+    room_list = Room.objects.all()
+    paginator = Paginator(room_list, 10, orphans=5)     # room object의 리스트와 per_page를 넣고 paginator 만들기, paginator 클래스 리턴함
+    page_obj = paginator.get_page(int(page_num))  # get_page(페이지가 음수 or 총 페이지보다 클 경우 마지막 페이지 리턴) or page(페이지 없으면 에러) 메서드는 page object를 리턴함
+    print(vars(page_obj.paginator))
     return render(
         request,
         "rooms/home.html",
-        context={
-            "rooms": all_rooms,
-            "page": page,
-            "page_count": page_count,
-            "page_range": page_range,
-        },
+        context={"page_obj": page_obj},
     )
