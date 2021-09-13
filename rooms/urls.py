2d4878030ac7bb46685a57f827c7b327cc943537
Oderystella <ddott59@gmail.com>
2021-09-13 12:33:01 +0900

    feat: create room_detail views using FBV

---
+++ rooms/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import room_detail
+
+
+app_name = "rooms"
+
+urlpatterns = [path("<int:pk>", room_detail, name="detail"),]
--- rooms/views.py
+++ rooms/views.py
@@ -1,4 +1,7 @@
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from django.views.generic import ListView
+from django.http import Http404
 from .models import Room
 
 
@@ -10,4 +13,13 @@ class HomeView(ListView):
     paginate_by = 10
     paginate_orphans = 5
     ordering = "created"
-    page_kwarg = "room"
+    context_object_name = "rooms"
+
+
+def room_detail(request, pk):
+    try:
+        room = Room.objects.get(pk=pk)
+        return render(request, "rooms/detail.html", {"room":room})
+
+    except Room.DoesNotExist:
+        return redirect(reverse("core:home"))
