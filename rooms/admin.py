73f49e6dc19f74737303d1c09de14c462eadfb57
Oderystella <ddott59@gmail.com>
2021-09-14 16:47:30 +0900

    feat: filtering with FBV

--- rooms/admin.py
+++ rooms/admin.py
@@ -64,6 +64,7 @@ class RoomAdmin(admin.ModelAdmin):
         "name",
         "country",
         "city",
+        "room_type",
         "price",
         "guests",
         "beds",
@@ -102,10 +103,10 @@ class RoomAdmin(admin.ModelAdmin):
     # 결과적으로 models.py에서 super().save() 해줘야 저장됨
     # 예를 들어, send_mail()코드를 넣으면 누군가 admin에서 모델 변경하면 메일이 날라오게 할 수도 있음
     def save_model(self, request, obj, form, change):
-        if (
-            obj.host != request.user
-        ):  # admin에서 room의 호스트와 로그인한 사람이 같을때만 모델 변경이 가능하게끔 조건 걸 수 있음
-            super().save_model(request, obj, form, change)  # obj.save() 함
+        # if (
+        #     obj.host != request.user
+        # ):  # admin에서 room의 호스트와 로그인한 사람이 같을때만 모델 변경이 가능하게끔 조건 걸 수 있음
+        super().save_model(request, obj, form, change)  # obj.save() 함
 
     # admin안의 함수는 2개의 파라미터를 가짐, self : RoomAdmin class, obj : room instance
     def count_amenities(self, obj):
--- rooms/views.py
+++ rooms/views.py
@@ -27,30 +27,30 @@ class RoomDetailView(DetailView):
 def search(request, **kwargs):
     city = request.GET.get("city", "Anywhere")
     city = city.title()
-    s_country = request.GET.get("country", "KR")
-    s_room_type = int(request.GET.get("room_type", 0))
+    country = request.GET.get("country", "KR")
+    room_type = int(request.GET.get("room_type", 0))
     price = int(request.GET.get("price", 0))
     guests = int(request.GET.get("guests", 0))
     bedrooms = int(request.GET.get("bedrooms", 0))
     beds = int(request.GET.get("beds", 0))
     baths = int(request.GET.get("baths", 0))
-    instant = request.GET.get("instant", False)
-    super_host = request.GET.get("super_host", False)
+    instant = bool(request.GET.get("instant", False))
+    superhost = bool(request.GET.get("superhost", False))
     s_amenities = request.GET.getlist("amenities")
     s_facilities = request.GET.getlist("facilities")
     s_house_rules = request.GET.getlist("house_rules")
 
     form = {
         "city": city,
-        "s_country": s_country,
-        "s_room_type": s_room_type,
+        "s_country": country,
+        "s_room_type": room_type,
         "price": price,
         "guests": guests,
         "bedrooms": bedrooms,
         "beds": beds,
         "baths": baths,
         "instant": instant,
-        "super_host": super_host,
+        "superhost": superhost,
         "s_amenities": s_amenities,
         "s_facilities": s_facilities,
         "s_house_rules": s_house_rules,
@@ -67,7 +67,52 @@ def search(request, **kwargs):
         "facilities": facilities,
         "house_rules": house_rules,
     }
-    return render(request, "rooms/search.html", {**form, **choices})
+    filter_args = {}
+
+    if city != "Anywhere":
+        filter_args["city__startswith"] = city
+    
+    filter_args["country"] = country
+
+    if room_type != 0:
+        filter_args["room_type__pk"] = room_type
+    
+    if price != 0:
+        filter_args["price__lte"] = price
+
+    if guests != 0:
+        filter_args["guests__gte"] = guests
+
+    if bedrooms != 0:
+        filter_args["bedrooms__gte"] = bedrooms
+
+    if beds != 0:
+        filter_args["beds__gte"] = beds
+
+    if baths != 0:
+        filter_args["baths__gte"] = baths
+
+    if instant is True:
+        filter_args["instant_book"] = True
+
+    if superhost is True:
+        filter_args["host__superhost"] = True
+
+    rooms = Room.objects.filter(**filter_args)
+    
+    if len(s_amenities) > 0:
+        for s_amenity in s_amenities:
+            # filter_args["amenities__pk"] = int(s_amenity)
+            rooms = rooms.filter(amenities__pk=int(s_amenity))
+        print(vars(rooms._query))
+
+    if len(s_facilities) > 0:
+        for s_facility in s_facilities:
+            # filter_args["facilities__pk"] = int(s_facility)
+            rooms = rooms.filter(facilities__pk=int(s_facility))
+
+    # rooms = Room.objects.filter(**filter_args)
+    return render(request, "rooms/search.html", {**form, **choices, "rooms": rooms})
 
 
 class EditRoomView(UpdateView):
