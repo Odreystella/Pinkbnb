6a42f624f886e0f0de4aba262182f7ee03e00eaf
Oderystella <ddott59@gmail.com>
2021-09-25 18:29:05 +0900

    fix: error handling

--- users/views.py
+++ users/views.py
@@ -87,6 +87,10 @@ def complete_verification(request, key):
     return redirect(reverse("core:home"))
 
 
+class GithubException(Exception):
+    pass
+
+
 # pinkbnb -> github
 def github_login(request):
     client_id = os.environ.get("GITHUB_CLIENT_ID")
@@ -94,54 +98,56 @@ def github_login(request):
     return redirect(f"https://github.com/login/oauth/authorize?client_id={client_id}&redirect_uri={redirect_uri}")
 
 
-# github -> pinkbnb
+# when user clicked accept, github -> pinkbnb
 def github_callback(request):
-    code = request.GET.get("code", None)
-    client_id = os.environ.get("GITHUB_CLIENT_ID")
-    client_secret = os.environ.get("GITHUB_CLIENT_SECRET")
-    if code is not None:                         # 1. callback으로 code 받았는지 확인
-        response = requests.post(
-            f"https://github.com/login/oauth/access_token?client_id={client_id}&client_secret={client_secret}&code={code}",
-            headers={"Accept": "application/json"}
-            )
-        response_json = response.json()
-        error = response_json.get("error", None)
-        if error is not None:                    # 2. 시간 초과, code 두번 이상 사용 등으로 error가 있다면 redirect
-            return redirect(reverse("users:login"))
-        else:                                    # 3. callback로 받은 code로 access_token 발급 받아서 user api에 접근하기  
-            access_token = response_json.get("access_token")
-            profile_request = requests.get(
-                "https://api.github.com/user", 
-                headers={
-                    "Authorization": f"token {access_token}",
-                    "Accept": "application/json"
-                },
-            )
-            profile_json = profile_request.json() # 4. user 정보 받음
-            username = profile_json.get("login", None)
-            if username is not None:              # 5. github에 user 정보가 있다면, name, email, bio 가져오기
-                name = profile_json.get("name")
-                email = profile_json.get("email")
-                bio = profile_json.get("bio")
-                try:                                     # 6. pinkbnb에 해당 이메일로 이미 유저가 존재하는데, 그 유저가 이미 github의 로그인 인증을 거쳤다면 로그인 시켜주기
-                    user = User.objects.get(email=email) 
-                    if user.login_method != User.LOGIN_GITHUB: 
-                        raise GithubException()                # 6-1. 다른 login_method로 만들어진 유저라면 에러 발생
-                              
-                except User.DoesNotExist:                # 7. pinkbnb에 해당 이메일의 유저가 없다면 만들어주기
-                    user = User.objects.create(
-                        username=email, 
-                        first_name=name, 
-                        email=email,
-                        bio=bio,
-                        login_method=User.LOGIN_GITHUB,
-                    )
-                    user.set_unusable_password()
-                    user.save()
-                login(request, user)
-                return redirect(reverse("core:home"))
-            else:
-                return redirect(reverse("users:login"))
-
-    else:
-        return redirect(reverse("users:login"))
+    try: 
+        code = request.GET.get("code", None)
+        client_id = os.environ.get("GITHUB_CLIENT_ID")
+        client_secret = os.environ.get("GITHUB_CLIENT_SECRET")
+        if code is not None:                         # 1. callback으로 code 받았는지 확인
+            token_request = requests.post(
+                f"https://github.com/login/oauth/access_token?client_id={client_id}&client_secret={client_secret}&code={code}",
+                headers={"Accept": "application/json"}
+                )
+            token_json = token_request.json()
+            error = token_json.get("error", None)
+            if error is not None:                    # 2. 시간 초과, code 두번 이상 사용 등으로 error가 있다면 redirect / json에 에러가 있는지 체크
+                raise GithubException()
+            else:                                    # 3. 에러가 없으면, callback로 받은 code로 access_token 발급 받아서  
+                access_token = token_json.get("access_token")
+                profile_request = requests.get(      # 3. github api에 requests 보내기
+                    "https://api.github.com/user", 
+                    headers={
+                        "Authorization": f"token {access_token}",
+                        "Accept": "application/json"
+                    },
+                )
+                profile_json = profile_request.json() # 4. response로 user 정보 받음
+                username = profile_json.get("login", None)
+                if username is not None:              # 5. github에 user 정보가 있다면, name, email, bio 가져오기
+                    name = profile_json.get("name")
+                    email = profile_json.get("email")
+                    bio = profile_json.get("bio")
+                    try:                                     # 6. pinkbnb에 해당 이메일로 이미 유저가 존재하는데, 그 유저가 이미 github의 로그인 인증을 거쳤다면 로그인 시켜주기
+                        user = User.objects.get(email=email) 
+                        if user.login_method != User.LOGIN_GITHUB: 
+                            raise GithubException()                # 6-1. 다른 login_method로 만들어진 유저라면 에러 발생
+                                
+                    except User.DoesNotExist:                # 7. pinkbnb에 해당 이메일의 유저가 없다면 만들어주기
+                        user = User.objects.create(
+                            username=email, 
+                            first_name=name, 
+                            email=email,
+                            bio=bio,
+                            login_method=User.LOGIN_GITHUB,
+                        )
+                        user.set_unusable_password()
+                        user.save()
+                    login(request, user)
+                    return redirect(reverse("core:home"))
+                else:                                        # 8. user 정보가 없다면 에러 발생
+                    raise GithubException()
+        else:
+            raise GithubException()
+    except GithubException:
+        return redirect(reverse("users:login"))     
