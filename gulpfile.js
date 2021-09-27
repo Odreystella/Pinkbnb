53f8b6e656445358388aafb08e322fb7bb2c0472
Oderystella <ddott59@gmail.com>
2021-09-27 17:38:01 +0900

    settings: add comments and STATICFILES_DIRS

--- gulpfile.js
+++ gulpfile.js
@@ -8,13 +8,13 @@ const css = () => {
     const minify = require("gulp-csso");
     sass.compiler = require("node-sass");
     return gulp
-    .src("assets/scss/styles.scss")
+    .src("assets/scss/styles.scss")    // .scss 파일 찾기
     .pipe(sass().on("error", sass.logError))
-    .pipe(postCSS([
+    .pipe(postCSS([                    // .scss 에 있는 코드들(@tailwindcss 등)을 css로 바꿈
         require("tailwindcss"),
         require("autoprefixer")]))
-    .pipe(minify())
-    .pipe(gulp.dest("static/css"));
+    .pipe(minify())                    // 파일 사이즈를 줄임
+    .pipe(gulp.dest("static/css"));    // pipe의 결과물을 static/css로 보냄
 };
 
 exports.default = css;
