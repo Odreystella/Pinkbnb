9e2a0a16d07accd2af8abeb2b4cab86a241d613f
Oderystella <ddott59@gmail.com>
2021-10-19 00:22:49 +0900

    feat: create favourite list

--- lists/views.py
+++ lists/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from rooms.models import Room
+from .models import List
 
-# Create your views here.
+
+def save_room(request, room_pk):
+    room = Room.objects.get_or_none(pk=room_pk)
+    if room is not None:
+        the_list, _ = List.objects.get_or_create(user=request.user, name="My Favourites Houses")
+        the_list.rooms.add(room) 
+    return redirect(reverse("rooms:detail", kwargs={"pk": room_pk}))
