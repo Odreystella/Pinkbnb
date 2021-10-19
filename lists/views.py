271ee3f78931f4c3307bd9cc408d893eb411e2bc
Oderystella <ddott59@gmail.com>
2021-10-19 13:17:34 +0900

    feat: create template tag for bookmark

--- lists/views.py
+++ lists/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 from rooms.models import Room
 from .models import List
 
 
-def save_room(request, room_pk):
+@login_required
+def toggle_room(request, room_pk):
+    action = request.GET.get("action", None)
     room = Room.objects.get_or_none(pk=room_pk)
-    if room is not None:
+    if room is not None and action is not None:
         the_list, _ = List.objects.get_or_create(user=request.user, name="My Favourites Houses")
-        the_list.rooms.add(room) 
+        if action == 'add':
+            the_list.rooms.add(room)
+        elif action == 'remove':
+            the_list.rooms.remove(room) 
     return redirect(reverse("rooms:detail", kwargs={"pk": room_pk}))
