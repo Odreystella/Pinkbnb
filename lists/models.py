6bc5ffa50f495d248a30afb4a6d169b589acc92d
Oderystella <ddott59@gmail.com>
2021-10-19 13:43:02 +0900

    fix: related_name

--- lists/models.py
+++ lists/models.py
@@ -7,7 +7,7 @@ class List(AbstractTimeStamped):
 
     name = models.CharField(max_length=80)
     user = models.OneToOneField(
-        "users.User", on_delete=models.CASCADE, related_name="lists"
+        "users.User", on_delete=models.CASCADE, related_name="list"
     )
     rooms = models.ManyToManyField("rooms.Room", blank=True, related_name="lists")
 
