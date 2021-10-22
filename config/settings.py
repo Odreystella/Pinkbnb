04eab1b7f8076626193bb641f42539f946fef573
Oderystella <ddott59@gmail.com>
2021-10-23 00:10:55 +0900

    fix: when DEBUG is False, override STATIC_ROOT

--- config/settings.py
+++ config/settings.py
@@ -205,6 +205,7 @@ if not DEBUG:
     
     AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com"
     STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static"   # s3에 업로드할 경로 오버라이딩
+    STATIC_ROOT = os.path.join(BASE_DIR, ".static_root")
 
     sentry_sdk.init(
     dsn=os.environ.get("SENTRY_URL"),
